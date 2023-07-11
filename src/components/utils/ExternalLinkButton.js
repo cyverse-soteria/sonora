d8e20e74badedec83e85fe512faf470f51f64950
mgwall17 <mgwall17@gmail.com>
2023-07-11 01:11:31 -0700

    Created custom external link button for banner

---
+++ src/components/utils/ExternalLinkButton.js
@@ -0,0 +1,25 @@
+/**
+ * A MUI Link for linking to external pages, opening in new tab.
+ *
+ * @author mwall
+ */
+import React from "react";
+import theme from "components/theme/default";
+
+import { Link, Button } from "@material-ui/core";
+
+const ExternalLinkButton = (props) => {
+    return (
+        <Button
+            fullWidth
+            style={{
+                margin: theme.spacing(0.4),
+            }}
+            size="large"
+        >
+            <Link target="_blank" rel="noopener noreferrer" {...props} />
+        </Button>
+    );
+};
+
+export default ExternalLinkButton;
